ad2026ebf4fd85c28af298727fb4a390dedfe54c
admin <admin@qq.com>
2023-09-07 13:46:33 +0800

    first commit

---
+++ src/main/java/com/example/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.git;
+
+/**
+ * @author Admin
+ * @date 2023/9/7 13:40
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!");
+    }
+}
